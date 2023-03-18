8fd8e3954bdf2e2d6229cc7f530e2e072866675c
Bhavishy M <bhavisshyya@Bhavishys-MacBook-Air.local>
2023-03-18 23:15:27 +0530

    server

---
+++ src/api/order/controllers/order.js
@@ -0,0 +1,64 @@
+"use strict";
+
+/**
+ * order controller
+ */
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    try {
+      const { products } = ctx.request.body;
+
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const productEntities = await strapi.entityService.findMany(
+            "api::product.product",
+            {
+              filter: {
+                key: product.key,
+              },
+            }
+          );
+
+          const realProduct = productEntities[0];
+          return {
+            price_data: {
+              currency: "inr",
+              product_data: {
+                name: product.title,
+                images: [product.image],
+              },
+              unit_amount: realProduct.price * 100,
+            },
+            quantity: product.quantity,
+          };
+        })
+      );
+
+      const session = await stripe.checkout.sessions.create({
+        shipping_address_collection: {
+          allowed_countries: ["IN"],
+        },
+        line_items: lineItems,
+        mode: "payment",
+        success_url: `${process.env.CLIENT_BASE_URL}/payments/success`,
+        cancel_url: `${process.env.CLIENT_BASE_URL}/payments/failed`,
+      });
+
+      await strapi.entityService.create("api::order.order", {
+        data: {
+          products,
+          stripeId: session.id,
+        },
+      });
+
+      return { stripeId: session.id };
+    } catch (error) {
+      ctx.response.status = 500;
+      return error;
+    }
+  },
+}));
